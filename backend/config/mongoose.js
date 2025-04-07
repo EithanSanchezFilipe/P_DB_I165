4884c85f91cb254fbc41a53fba0266f999f30b47
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-04-07 10:43:40 +0200

    feat: update MongoDB connection string to use environment variable

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://root:admin@localhost:27017/db_todoapp?authSource=admin', {});
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log('Connexion réussie à MongoDB');
   } catch (error) {
     console.error('Erreur de connexion :', error);
