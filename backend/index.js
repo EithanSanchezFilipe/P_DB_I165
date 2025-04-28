61d8a26ac4ec84bfcbe08061ad708c20ddb5bf90
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-04-28 08:26:53 +0200

    fix: not taking the mongo uri from de .env

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const dotenv = require('dotenv').config();
 const cookieParser = require('cookie-parser');
 
 const app = express();
